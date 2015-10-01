be1237782213b7e811690d55667ee19708bdf667
Steven Proctor <steven.proctor@gmail.com>
2015-10-01 10:25:24 -0500

    Lab1 completed

---
+++ lab1/lab1.exs
@@ -0,0 +1,19 @@
+defmodule Lab1 do
+  def first_and_third([first, _second, third | _rest]) do
+    {first, third}
+  end
+
+  def min(list) do
+    Enum.reduce list, &min/2
+  end
+
+  def avg(list) do
+    {total, count} = Enum.reduce list, {0, 0}, fn (item, {total, count}) -> {total + item, count+1} end
+    total/count
+  end
+
+  def num_atoms(list) do
+    list
+      |> Enum.count &is_atom/1
+  end
+end
